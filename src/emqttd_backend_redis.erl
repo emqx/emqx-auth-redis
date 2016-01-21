c96a84dfc8512aef750977696d19a929a7c8793e
Feng <ery.lee@gmail.com>
2016-01-21 15:32:20 +0800

    loadsub

--- src/emqttd_backend_redis.erl
+++ src/emqttd_backend_redis.erl
@@ -28,9 +28,12 @@
 
 -include("../../../include/emqttd.hrl").
 
+-include("../../../include/emqttd_protocol.hrl").
+
 -export([load/0, unload/0]).
 
 -export([on_client_subscribe_after/3,
+         on_client_connected/3,
          on_client_unsubscribe/3]).
 
 %% Called when the plugin application start
@@ -39,11 +42,14 @@ load() ->
                 emqttd_broker:hook('client.subscribe.after', {?MODULE, on_client_subscribe_after},
                                    {?MODULE, on_client_subscribe_after, [SubCmd]})
         end),
+    with_cmd_enabled(loadsub, fun(LoadCmd) ->
+                emqttd_broker:hook('client.connected', {?MODULE, on_client_connected},
+                                   {?MODULE, on_client_connected, [LoadCmd]})
+        end),
     with_cmd_enabled(unsubcmd, fun(UnsubCmd) ->
                 emqttd_broker:hook('client.unsubscribe', {?MODULE, on_client_unsubscribe},
                                    {?MODULE, on_client_unsubscribe, [UnsubCmd]})
         end).
-
 
 on_client_subscribe_after(ClientId, TopicTable, SubCmd) ->
     with_username(ClientId, fun(Username) ->
@@ -51,6 +57,17 @@ on_client_subscribe_after(ClientId, TopicTable, SubCmd) ->
                 Res = emqttd_redis_client:query(repl_var(SubCmd, Username) ++ TopicTable),
                 io:format("Result: ~p~n", [Res])
         end).
+
+on_client_connected(?CONNACK_ACCEPT, #mqtt_client{username = undefined}, _LoadCmd) ->
+    ok;
+
+on_client_connected(?CONNACK_ACCEPT, #mqtt_client{username = Username}, LoadCmd) ->
+    case emqttd_redis_client:query(repl_var(LoadCmd, Username)) of
+        {ok, Values} -> io:format("Values: ~p~n", [Values]);
+        {error, Error} -> io:format("Error: ~p~n", [Error])
+    end;
+
+on_client_connected(_ConnAck, _Client) -> ok.
 
 on_client_unsubscribe(ClientId, Topics, UnsubCmd) ->
     with_username(ClientId, fun(Username) ->
@@ -62,6 +79,7 @@ on_client_unsubscribe(ClientId, Topics, UnsubCmd) ->
 %% Called when the plugin application stop
 unload() ->
     emqttd_broker:unhook('client.subscribe.after', {?MODULE, on_client_subscribe_after}),
+    emqttd_broker:unhook('client.connected',       {?MODULE, on_client_connected}),
     emqttd_broker:unhook('client.unsubscribe',     {?MODULE, on_client_unsubscribe}).
 
 with_cmd_enabled(Name, Fun) ->
