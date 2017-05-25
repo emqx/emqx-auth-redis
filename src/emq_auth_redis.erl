c87e22dac860f84e8b1b3244f8981f619d827bd0
turtled <turtled@emqtt.io>
2017-05-25 13:58:52 +0800

    Format code

--- src/emq_auth_redis.erl
+++ src/emq_auth_redis.erl
@@ -39,13 +39,13 @@ check(Client, Password, #state{auth_cmd  = AuthCmd,
                                super_cmd = SuperCmd,
                                hash_type = HashType}) ->
     Result = case emq_auth_redis_cli:q(AuthCmd, Client) of
-                {ok, PassHash} when is_binary(Password)->
+                {ok, PassHash} when is_binary(PassHash) ->
                     check_pass(PassHash, Password, HashType);  
                 {ok, [undefined|_]} ->
                     ignore;
                 {ok, [PassHash]} ->
                     check_pass(PassHash, Password, HashType);   
-                {ok, [PassHash,Salt|_]} ->
+                {ok, [PassHash, Salt|_]} ->
                     check_pass(PassHash, Salt, Password, HashType);
                 {error, Reason} ->
                     {error, Reason}
