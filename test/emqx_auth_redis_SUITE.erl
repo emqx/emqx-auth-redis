4ebf1dab7cd8827b79b37bcd5ff4ded37845bf18
terry-xiaoyu <506895667@qq.com>
2018-11-10 15:40:29 +0800

    Change the emqx_client:start_link/1 API

--- test/emqx_auth_redis_SUITE.erl
+++ test/emqx_auth_redis_SUITE.erl
@@ -136,10 +136,11 @@ check_acl(_Config) ->
 
 acl_super(_Config) ->
     reload([{password_hash, plain}]),
-    {ok, C, _} = emqx_client:start_link([{host,      "localhost"},
+    {ok, C} = emqx_client:start_link([{host,      "localhost"},
                                       {client_id, <<"simpleClient">>},
                                       {username,  <<"plain">>},
                                       {password,  <<"plain">>}]),
+    {ok, _} = emqx_client:connect(C),
     timer:sleep(10),
     emqx_client:subscribe(C, <<"TopicA">>, qos2),
     timer:sleep(1000),
