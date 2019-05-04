9ba2a96a0181e6f8ed3da200c703739f9f3c58b8
spring2maz <spring2maz@gmail.com>
2019-05-04 17:41:56 +0200

    Switch to build with rebar3

--- make-config.sh
+++ make-config.sh
@@ -1,8 +1,4 @@
 #!/bin/bash -e
 
-pushd ${REBAR_BUILD_DIR}/lib/cuttlefish
-make
-popd
-
 ${REBAR_BUILD_DIR}/lib/cuttlefish/cuttlefish -l info -e etc/ -c etc/emqx_auth_redis.conf -i priv/emqx_auth_redis.schema -d data
 
--- test/emqx_auth_redis_SUITE.erl
+++ test/emqx_auth_redis_SUITE.erl
@@ -51,12 +51,7 @@ groups() ->
      ].
 
 init_per_suite(Config) ->
-    [start_apps(App, {SchemaFile, ConfigFile}) ||
-        {App, SchemaFile, ConfigFile}
-            <- [{emqx, local_path("deps/emqx/priv/emqx.schema"),
-                       local_path("deps/emqx/etc/emqx.conf")},
-                {emqx_auth_redis, local_path("priv/emqx_auth_redis.schema"),
-                                  local_path("etc/emqx_auth_redis.conf")}]],
+    emqx_ct_helpers:start_apps([emqx, emqx_auth_redis], fun set_special_configs/1),
     Config.
 
 end_per_suite(_Config) ->
@@ -68,34 +63,14 @@ end_per_suite(_Config) ->
     application:stop(emqx_auth_redis),
     application:stop(ecpool).
 
-get_base_dir() ->
-    {file, Here} = code:is_loaded(?MODULE),
-    filename:dirname(filename:dirname(Here)).
-
-local_path(RelativePath) ->
-    filename:join([get_base_dir(), RelativePath]).
-
-start_apps(App, {SchemaFile, ConfigFile}) ->
-    read_schema_configs(App, {SchemaFile, ConfigFile}),
-    set_special_configs(App),
-    application:ensure_all_started(App).
-
-read_schema_configs(App, {SchemaFile, ConfigFile}) ->
-    ct:pal("Read configs - SchemaFile: ~p, ConfigFile: ~p", [SchemaFile, ConfigFile]),
-    Schema = cuttlefish_schema:files([SchemaFile]),
-    Conf = conf_parse:file(ConfigFile),
-    NewConfig = cuttlefish_generator:map(Schema, Conf),
-    Vals = proplists:get_value(App, NewConfig, []),
-    [application:set_env(App, Par, Value) || {Par, Value} <- Vals].
-
 set_special_configs(emqx) ->
     application:set_env(emqx, allow_anonymous, false),
     application:set_env(emqx, acl_nomatch, deny),
     application:set_env(emqx, acl_file,
-                        local_path("deps/emqx/test/emqx_SUITE_data/acl.conf")),
+                        emqx_ct_helpers:deps_path(emqx, "test/emqx_SUITE_data/acl.conf")),
     application:set_env(emqx, enable_acl_cache, false),
     application:set_env(emqx, plugins_loaded_file,
-                        local_path("deps/emqx/test/emqx_SUITE_data/loaded_plugins"));
+                        emqx_ct_helpers:deps_path(emqx, "test/emqx_SUITE_data/loaded_plugins"));
 set_special_configs(_App) ->
     ok.
 
