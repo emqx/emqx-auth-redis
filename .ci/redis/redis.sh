0fb50696e3778696eccc075b50ff36b8c2eac0c0
z8674558 <own7000hr@gmail.com>
2020-12-12 16:52:02 +0900

    chore(ci): refactor redis.sh

--- .ci/redis/redis.sh
+++ .ci/redis/redis.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
-tls=false;
-while getopts n:t OPT
+node=single
+tls=false
+while [[ $# -gt 0 ]]
 do
-    case $OPT in
-        n)  nodes=$OPTARG
-            ;;
-        t)  tls=true
-            ;;
-        \?) exit
-            ;;
-    esac
+key="$1"
+
+case $key in
+    -n|--node)
+    node="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -t|--tls-enabled)
+    tls="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    *)
+    shift # past argument
+    ;;
+esac
 done
 
 rm -f \
@@ -21,60 +31,27 @@ rm -f \
     /data/conf/nodes.7001.conf \
     /data/conf/nodes.7002.conf ;
 
-if [ ${nodes} = "cluster" ] ; then
+if [ ${node} = "cluster" ] ; then
   if $tls ; then
-    redis-server --cluster-enabled yes --port 7000 --cluster-config-file /data/conf/nodes.7000.conf --daemonize yes \
-                                       --tls-port 8000 \
-                                       --tls-cert-file /tls/redis.crt \
-                                       --tls-key-file /tls/redis.key \
-                                       --tls-ca-cert-file /tls/ca.crt \
-                                       --bind 0.0.0.0 ::
-    redis-server --cluster-enabled yes --port 7001 --cluster-config-file /data/conf/nodes.7001.conf --daemonize yes \
-                                       --tls-port 8001 \
-                                       --tls-cert-file /tls/redis.crt \
-                                       --tls-key-file /tls/redis.key \
-                                       --tls-ca-cert-file /tls/ca.crt \
-                                       --bind 0.0.0.0 ::
-    redis-server --cluster-enabled yes --port 7002 --cluster-config-file /data/conf/nodes.7002.conf --daemonize yes \
-                                       --tls-port 8002 \
-                                       --tls-cert-file /tls/redis.crt \
-                                       --tls-key-file /tls/redis.key \
-                                       --tls-ca-cert-file /tls/ca.crt \
-                                       --bind 0.0.0.0 ::
+    redis-server /data/conf/redis-tls.conf --port 7000 --cluster-config-file /data/conf/nodes.7000.conf \
+                                           --tls-port 8000 --cluster-enabled yes ;
+    redis-server /data/conf/redis-tls.conf --port 7001 --cluster-config-file /data/conf/nodes.7001.conf \
+                                           --tls-port 8001 --cluster-enabled yes;
+    redis-server /data/conf/redis-tls.conf --port 7002 --cluster-config-file /data/conf/nodes.7002.conf \
+                                           --tls-port 8002 --cluster-enabled yes;
   else
-    redis-server --cluster-enabled yes --port 7000 --cluster-config-file /data/conf/nodes.7000.conf --daemonize yes --bind 0.0.0.0 ::
-    redis-server --cluster-enabled yes --port 7001 --cluster-config-file /data/conf/nodes.7001.conf --daemonize yes --bind 0.0.0.0 ::
-    redis-server --cluster-enabled yes --port 7002 --cluster-config-file /data/conf/nodes.7002.conf --daemonize yes --bind 0.0.0.0 ::
+    redis-server /data/conf/redis.conf --port 7000 --cluster-config-file /data/conf/nodes.7000.conf --cluster-enabled yes;
+    redis-server /data/conf/redis.conf --port 7001 --cluster-config-file /data/conf/nodes.7001.conf --cluster-enabled yes;
+    redis-server /data/conf/redis.conf --port 7002 --cluster-config-file /data/conf/nodes.7002.conf --cluster-enabled yes;
   fi
-elif [ ${nodes} = "sentinel" ] ; then
-  if $tls ; then
-    redis-server --cluster-enabled no  --port 7000 --cluster-config-file /data/conf/nodes.7000.conf --daemonize yes \
-                                       --tls-port 8000 \
-                                       --tls-cert-file /tls/redis.crt \
-                                       --tls-key-file /tls/redis.key \
-                                       --tls-ca-cert-file /tls/ca.crt \
-                                       --bind 0.0.0.0 ::
-    redis-server --cluster-enabled no  --port 7001 --cluster-config-file /data/conf/nodes.7001.conf --daemonize yes \
-                                       --tls-port 8001 \
-                                       --tls-cert-file /tls/redis.crt \
-                                       --tls-key-file /tls/redis.key \
-                                       --tls-ca-cert-file /tls/ca.crt \
-                                       --bind 0.0.0.0 :: \
-                                       --slaveof 172.16.239.10 7000
-    redis-server --cluster-enabled no  --port 7002 --cluster-config-file /data/conf/nodes.7002.conf --daemonize yes \
-                                       --tls-port 8002 \
-                                       --tls-cert-file /tls/redis.crt \
-                                       --tls-key-file /tls/redis.key \
-                                       --tls-ca-cert-file /tls/ca.crt \
-                                       --bind 0.0.0.0 :: \
-                                       --slaveof 172.16.239.10 7000
-  else
-    redis-server --cluster-enabled no --port 7000 --cluster-config-file /data/conf/nodes.7000.conf --daemonize yes --bind 0.0.0.0 ::
-    redis-server --cluster-enabled no --port 7001 --cluster-config-file /data/conf/nodes.7001.conf --daemonize yes --bind 0.0.0.0 :: --slaveof 172.16.239.10 7000
-    redis-server --cluster-enabled no --port 7002 --cluster-config-file /data/conf/nodes.7002.conf --daemonize yes --bind 0.0.0.0 :: --slaveof 172.16.239.10 7000
-  fi
+elif [ ${node} = "sentinel" ] ; then
+    redis-server /data/conf/redis.conf --port 7000 --cluster-config-file /data/conf/nodes.7000.conf \
+                                       --cluster-enabled no;
+    redis-server /data/conf/redis.conf --port 7001 --cluster-config-file /data/conf/nodes.7001.conf \
+                                       --cluster-enabled no --slaveof 172.16.239.10 7000;
+    redis-server /data/conf/redis.conf --port 7002 --cluster-config-file /data/conf/nodes.7002.conf \
+                                       --cluster-enabled no --slaveof 172.16.239.10 7000;
 fi
-
 REDIS_LOAD_FLG=true;
 
 while $REDIS_LOAD_FLG;
@@ -98,9 +75,9 @@ do
     else
         continue;
     fi
-    if [ ${nodes} = "cluster" ] ; then
+    if [ ${node} = "cluster" ] ; then
       yes "yes" | redis-cli --cluster create 172.16.239.10:7000 172.16.239.10:7001 172.16.239.10:7002;
-    elif [ ${nodes} = "sentinel" ] ; then
+    elif [ ${node} = "sentinel" ] ; then
       cp /data/conf/sentinel.conf /_sentinel.conf
       redis-server /_sentinel.conf --sentinel;
     fi
